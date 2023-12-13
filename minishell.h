fee79dd058ce8aa2043316ed40940d2ef6ab4142
Hyeongseop Shin <hyeongsh@c2r1s7.42seoul.kr>
2023-12-13 22:15:37 +0900

    new git

--- minishell.h
+++ minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hyeongsh <hyeongsh@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jongmlee <jongmlee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 16:53:34 by hyeongsh          #+#    #+#             */
-/*   Updated: 2023/12/06 18:35:24 by hyeongsh         ###   ########.fr       */
+/*   Updated: 2023/12/13 21:56:51 by jongmlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define MINISHELL_H
 
 # include "libft.h"
-# include <stdio.h>
+# include <unistd.h>
 # include <readline/readline.h>
 # include <readline/history.h>
 # include <sys/wait.h>
@@ -27,43 +27,105 @@
 # include <termios.h>
 # include <term.h>
 # include <fcntl.h>
-# include <string.h>
-# include <errno.h>
 
-// pipe
+# define PATH "PATH="
+# define BASIC_PATH "/usr/local/bin:/usr/bin:/bin:\
+	/usr/sbin:/sbin:/usr/local/munki:/Library/Apple/usr/bin"
+# define CMD_NOT_FOUND_ERR ": command not found\n"
+# define HEREDOC "here_doc"
+
+struct sigaction	sig;
+
+typedef struct s_container
+{
+	struct s_data	*head;
+	char			**envp;
+	int				cnt;
+}	t_container;
+
 typedef struct s_data
 {
-	char	**path;
-	char	**cmd;
-	int		**pipe;
-	pid_t	*id;
-	int		cmd_num;
+	char			*infile;
+	char			*outfile;
+	char			*delimeter;
+	char			**cmd_arr;
+	int				is_append;
+	struct s_data	*next;
 }	t_data;
 
-int		pipe(int ac, char **av, char **env);
-void	error_print(int flag);
-char	**path_setting(char **env);
-char	*path_search(char **env, char *command);
-int		open_check(int ac, char **av, int *fd, char **heredoc);
-void	first_setting(int ac, char **av, t_data *data, char **env);
-void	error_print(int flag);
-void	make_child(t_data *data, char **av, int i, char **env);
-void	command_exec_first(t_data *data, char **av, char **env);
-void	command_exec_mid(t_data *data, int i, char **env);
-void	command_exec_end(t_data *data, char **env);
-void	close_pipe(t_data *data, int i);
-void	make_heredoc(char **av, char **heredoc);
-void	heredoc_setting(int ac, char **av, t_data *data, char **env);
-int		ending(t_data *data, char *heredoc);
-void	ending_free(t_data *data);
-char	**make_command(char *cmd);
-char	**xfile_split(char *cmd);
-char	**cmd_split(char *cmd, char sep);
-int		cmd_countword(char *cmd, char sep);
-void	cmd_input(char *toss, char **cmd, char sep);
-void	make_child_heredoc(t_data *data, char *heredoc, char **env);
-void	command_exec_first_heredoc(t_data *data, char *heredoc, char **env);
-void	command_exec_end_heredoc(t_data *data, char **env);
-void	command_error(char *command);
+typedef struct s_info
+{
+	int		pipe_fds[2][2];
+	pid_t	last_pid;
+	int		infile_fd;
+	int		outfile_fd;
+	int		idx;
+	int		cur;
+	int		is_heredoc;
+	t_data	*data;
+}	t_info;
+
+t_list	*pasing(char *line);
+void	split_free(char **command);
+
+int		ms_init(char c, char *s);
+int		ms_split_input(char *toss, char **cmd, char *oper);
+int		ms_split_first(char *toss, char **cmd, char *oper);
+int		ms_split_plus(char *toss, int *i, char **cmd);
+char	**ms_split(char *cmd);
+
+/* debug */
+void	print_2d_arr(char **s);
+void	print_3d_arr(char ***s);
+void	print_node(t_list *list);
+void	print_data(t_data *data);
+void	print_data_node(t_data *data);
+void	print_container(t_container *con);
+
+/* init_data */
+char	**make_cmd(t_list *list);
+int		get_cmd_arr_len(t_list *lst);
+t_list	*check_type_and_move_list(t_data *data, t_list *list);
+char	***make_cmd_arr(t_data *data, t_list *list);
+void	init_data(t_data *data, t_list *list, char **envp);
+
+/* ms_utils1 */
+char	**ms_2d_arr_dup(char **s);
+
+/* pipe_parse */
+char	*get_path(char **envp);
+char	*make_cmd_path(char const *path, char const *cmd);
+char	*get_valid_path(char **cmds, char *env_path);
+int		execute_cmd(t_info *info);
+
+/* pipe_exit */
+void	free_2d_array(char **arr);
+void	perror_exit(char *str, int exit_code);
+
+/* pipe_utils */
+void	open_pipe(t_info *info);
+void	close_pipe(t_info *info);
+void	open_file(t_info *info);
+void	close_all_pipe(t_info *info);
+void	dup2_sub(int first, int second);
+void	redirect(t_info *info);
+
+/* pipe */
+int		wait_children(t_info *info);
+void	init_info(t_info *info, t_data *data);
+void	child(t_info *info);
+void	here_doc(t_info *info);
+int		pipex(t_data *data);
+
+/* data_list */
+void	init_data_node(t_data *node);
+int		get_cmd_arr_len(t_list *lst);
+t_data	*data_lstnew(t_list *line);
+t_data	*data_lstlast(t_data *lst);
+void	data_lstadd_back(t_data **lst, t_data *new);
+t_data	*make_data_list(t_list *line);
+int	get_data_list_len(t_data *lst);
+void	init_container(t_container *con, t_list *line, char **envp);
+
 
 #endif
